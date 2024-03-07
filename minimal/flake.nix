4fb1d7ac5cc8b62f521f111f4cf8e1d69bf0e489
Josh <josh@joshj.dev>
2024-03-07 17:37:31 +0000

    add firefox addons flake

--- minimal/flake.nix
+++ minimal/flake.nix
@@ -15,6 +15,11 @@
     # Shameless plug: looking for a way to nixify your themes and make
     # everything match nicely? Try nix-colors!
     # nix-colors.url = "github:misterio77/nix-colors";
+
+    firefox-addons = {
+      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
