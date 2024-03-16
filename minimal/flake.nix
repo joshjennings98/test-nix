dc3dbf97f3895edae95ef3ebc50526e790838083
Josh <josh@joshj.dev>
2024-03-16 17:03:40 +0000

    Add impermance to flake.nix

--- minimal/flake.nix
+++ minimal/flake.nix
@@ -9,6 +9,8 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    impermanence.url = "github:nix-community/impermanence";
+
     firefox-addons = {
       url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
       inputs.nixpkgs.follows = "nixpkgs";
