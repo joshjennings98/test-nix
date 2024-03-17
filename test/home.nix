b9d1e5ea0667c6639ce668faccd76b850fc3575d
Josh <josh@joshj.dev>
2024-03-17 11:59:49 +0000

    test

--- test/home.nix
+++ test/home.nix
@@ -1,7 +1,9 @@
 { pkgs, inputs, ... }:
 
 { 
-  imports = [ ];
+  imports = [
+
+  ];
 
   home.stateVersion = "23.11"; # Please read the comment before changing.
 
@@ -9,6 +11,26 @@
     username = "josh";
     homeDirectory = "/home/josh";
   };
+
+  fileSystems."/persist".neededForBoot = true;
+  environment.persistence."/persist/system" = {
+    hideMounts = true;
+    directories = [
+      "/etc/nixos"
+      "/var/log"
+      "/var/lib/bluetooth"
+      "/var/lib/nixos"
+      "/var/lib/systemd/coredump"
+      "/etc/NetworkManager/system-connections"
+      { directory = "/var/lib/colord"; user = "colord"; group = "colord"; mode = "u=rwx,g=rx,o="; }
+    ];
+    files = [
+      "/etc/machine-id"
+      { file = "/var/keys/secret_file"; parentDirectory = { mode = "u=rwx,g=,o="; }; }
+    ];
+  };
+
+  programs.fuse.userAllowOther = true;
 
   programs.home-manager.enable = true;
 }
