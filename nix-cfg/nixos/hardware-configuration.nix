639146e48a21410b5f1a212324fcac225697af88
Josh <josh@joshj.dev>
2024-03-19 21:08:38 +0000

    Update hardware-configuration.nix

--- nix-cfg/nixos/hardware-configuration.nix
+++ nix-cfg/nixos/hardware-configuration.nix
@@ -1,10 +1,3 @@
-# This is just an example, you should generate yours with nixos-generate-config and put it in here.
 {
-  fileSystems."/" = {
-    device = "/dev/sda1";
-    fsType = "ext4";
-  };
-
-  # Set your system kind (needed for flakes)
-  nixpkgs.hostPlatform = "x86_64-linux";
+  throw "This file needs to be created with `sudo nixos-generate-config --no-filesystems --root /mnt` and copied here."
 }
