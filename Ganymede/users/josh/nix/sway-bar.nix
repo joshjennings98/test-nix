42e0bbcd1f44132dc89a27565f2656054d5a14e3
Josh <josh@joshj.dev>
2024-04-02 19:34:57 +0100

    fix path to barista

--- Ganymede/users/josh/nix/sway-bar.nix
+++ Ganymede/users/josh/nix/sway-bar.nix
@@ -6,7 +6,7 @@ pkgs.buildGoModule rec {
     nativeBuildInputs = [ pkgs.pkg-config ];
     buildInputs = [ pkgs.alsaLib ];
     vendorHash = "sha256-yqPgFhoibZoCkBF7XzDFgZS7ScqsAEPCcX7baknRO1E=";
-    src = ./barista;  
+    src = ../barista;  
     meta = with pkgs.lib; {
         description = "i3/sway bar created using Go with 'github.com/soumya92/barista'";
     };
